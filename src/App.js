ef38412bb65ea537c08794fbc1e567d4954be7aa
Maxwell Christ <54086466+MaxwellChrist@users.noreply.github.com>
2022-04-12 10:08:41 -0400

    Got list items to show upon button click

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
-import { people } from './people';
+import { people } from "./people";
 console.log(people);
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     console.log("constructor invoked");
     this.state = {
-      friends: people,
-      friendslist: ""
-    }
+      friends: people
+    };
   }
 
   viewFriends = (e) => {
-    console.log(this.state.friends[0].friends)
-    console.log(e.target.innerHTML)
-    this.state.friends.map(f => {
-      if (e.target.innerHTML.includes(f.first_name)) {
-        return (
-          f.friends.map(person => {
-            <h1>{person.firstName}</h1>
-        }))
+    let appendTarget = document.getElementById("target");
+    this.state.friends.map((f) => {
+      if (e.target.textContent.includes(f.first_name)) {
+        return f.friends.map((person, index) => {
+          let newValue = document.createElement("li");
+          newValue.id = index;
+          newValue.textContent = `${person.firstName} ${person.lastName}`;
+          appendTarget.appendChild(newValue);
+        });
       }
-    })
-  }
+    });
+  };
 
-  seeFriends = (e) => {
-    console.log(e.target)
+  componentDidMount() {
+    console.log("CDM invoked!");
   }
 
   render() {
@@ -35,10 +35,15 @@ class App extends Component {
     return (
       <div className="App">
         <h1> Welcome to Friends! </h1>
-        {this.state.friends.map(friend => (
+        {this.state.friends.map((friend) => (
           <>
             <div key={friend.id}>{friend.first_name}</div>
-              <button onClick={this.viewFriends}>Click her to see all of {friend.first_name}'s' friends</button>
+            <button onClick={this.viewFriends}>
+              Click her to see all of {friend.first_name}'s' friends
+            </button>
+            <ul id="target">
+              This is the list of {friend.first_name}'s' friends
+            </ul>
           </>
         ))}
       </div>
